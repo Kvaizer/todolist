9b78cb85ca6bc66e004bfe7dfbf85be5ebc79d0c
Kvaizer <kvaizer999@mail.ru>
2022-08-14 14:42:24 +0300

    Updates

---
+++ precache-manifest.301d93d0208ec238ca9c6ee72807333f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe23f8ef6f46659a4979e65956ebaf6e",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "f485ca83eb70428637f0",
+    "url": "/todolist/static/css/main.674b6137.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/todolist/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f485ca83eb70428637f0",
+    "url": "/todolist/static/js/main.f686d716.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
